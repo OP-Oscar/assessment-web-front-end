c9ade8b98b2383846b90572ed8f90da42435cf7e
OP-Oscar <oscarpena07@outlook.com>
2023-04-23 23:33:35 -0500

    update 3

--- neighborhood.js
+++ neighborhood.js
@@ -1,3 +1,15 @@
 // console.log('Neighborhood JS successfully linked')
 
 const ranBtn = document.querySelector("#ranBtn")
+
+const resta = ["Killen's Barbecue", "Boil House", "My Bistro Noodle & Grill", "Downtown Aquarium Restaurant"];
+
+
+function ranRest(evt){
+	evt.preventDefault();
+    const random = Math.floor(Math.random() * resta.length);
+    alert(`You should go to ${resta[random]}`)
+}
+
+
+ranBtn.addEventListener("click",ranRest)
